e8b7e0a80af90fb06b429e54c94de4386e8b06b7
Ignacio Abejaro <ignacio.abejaro@ticxcat.cat>
2020-09-17 07:03:58 +0200

    Updated information URLs

--- includes/admin/components/networks/wsl.components.networks.setup.php
+++ includes/admin/components/networks/wsl.components.networks.setup.php
@@ -289,8 +289,9 @@ function wsl_component_networks_setup()
 				</div>
             <!--// XTEC ************ AFEGIT - Customize help info for Moodle and Google providers. Not using gettext, sorry! :( -->
             <!--// 2014.11.19 @aginard -->
+            <!--// 2020.10.15 @nacho: modified URLS -->
         <?php else : global $isAgora; if ($provider_id == 'Moodle') : ?>
-            <?php $help_moodle= ($isAgora) ? 'http://agora.xtec.cat/moodle/moodle/mod/glossary/view.php?id=1741&mode=entry&hook=2228':'https://sites.google.com/a/xtec.cat/ajudaxtecblocs/usuaris/configuracio-d-acces-a-xtecblocs-amb-els-usuaris-del-moodle'; ?>
+            <?php $help_moodle= ($isAgora) ? 'https://educaciodigital.cat/moodle/moodle/mod/glossary/view.php?id=1741&mode=entry&hook=2228':'https://sites.google.com/a/xtec.cat/ajudaxtecblocs/usuaris/configuracio-d-acces-a-xtecblocs-amb-els-usuaris-del-moodle'; ?>
             <div
                     class="wsl_div_settings_help_<?php echo $provider_id; ?>"
                     style="<?php if( isset( $_REQUEST["enable"] ) && ! isset( $_REQUEST["settings-updated"] ) && $_REQUEST["enable"] == $provider_id ) echo "-"; // <= lolz ?>display:none;">
@@ -298,7 +299,7 @@ function wsl_component_networks_setup()
                 <p>Per fer que els usuaris del Moodle puguin entrar a aquest espai automàticament, visiteu <a href="<?php echo $help_moodle?>" target="_blank">aquesta pàgina</a> i seguiu les instruccions.</p><p>La primera vegada que un usuari/ària del Moodle entri, se li crearà automàticament un compte a aquest espai en cas que no en tingui.</p>
             </div>
         <?php endif; if ($provider_id == 'Google') : ?>
-            <?php $help_google= ($isAgora) ? 'http://agora.xtec.cat/moodle/moodle/mod/glossary/view.php?id=1741&mode=entry&hook=2281':'https://sites.google.com/a/xtec.cat/ajudaxtecblocs/usuaris/configuracio-d-acces-a-xtecblocs-amb-usuaris-de-google'; ?>
+            <?php $help_google= ($isAgora) ? 'https://ateneu.xtec.cat/wikiform/wikiexport/cmd/tac/agora-nodes/usuaris/validacio/google':'https://sites.google.com/a/xtec.cat/ajudaxtecblocs/usuaris/configuracio-d-acces-a-xtecblocs-amb-usuaris-de-google'; ?>
             <div
                     class="wsl_div_settings_help_<?php echo $provider_id; ?>"
                     style="<?php if( isset( $_REQUEST["enable"] ) && ! isset( $_REQUEST["settings-updated"] ) && $_REQUEST["enable"] == $provider_id ) echo "-"; // <= lolz ?>display:none;">
